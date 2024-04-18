cbcf152a8b83f160036c1366d76386d1a871a523
cauematheus23 <cauematheus_23@hotmail.com>
2024-04-18 19:40:51 -0300

    program commit

---
+++ ParImpar/Program.cs
@@ -0,0 +1,53 @@
+﻿int ni = 0, nf = 0, c = 0, npi;
+Console.WriteLine("Digite o valor inicial: ");
+ni = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o valor final: ");
+nf = int.Parse(Console.ReadLine());
+
+
+if (ni == nf)
+{
+    Console.WriteLine("Por favor digite valores diferentes");
+}
+else
+{
+    if (ni > nf)
+    {
+        c = ni;
+        npi = ni;
+        while (c > nf + 1)
+        {
+            npi--;
+            c--;
+            if ((npi % 2) == 0)
+            {
+                Console.WriteLine($"{npi} é par");
+            }
+            else
+            {
+                Console.WriteLine($"{npi} é impar");
+            }
+
+        }
+    }
+    else
+    {
+        c = ni;
+        npi = ni;
+        while (c < nf - 1)
+        {
+            npi++;
+            c++;
+            if ((npi % 2) == 0)
+            {
+                Console.WriteLine($"{npi} é par");
+            }
+            else
+            {
+                Console.WriteLine($"{npi} é impar");
+            }
+        }
+    }
+}
+
+
